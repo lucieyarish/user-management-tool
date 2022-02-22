0ee7782780dfd90b994cb90d8f85e0ff7c677050
Lucie Yarish <lucieyarish@hotmail.com>
2022-02-22 23:50:16 +0100

    create user model and switch to h2 db

--- src/main/java/com/lucieyarish/usermanagementtool/UserManagementToolApplication.java
+++ src/main/java/com/lucieyarish/usermanagementtool/UserManagementToolApplication.java
@@ -1,13 +1,22 @@
 package com.lucieyarish.usermanagementtool;
 
+import com.lucieyarish.usermanagementtool.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserManagementToolApplication {
 
+    private final UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        addSampleData();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(UserManagementToolApplication.class, args);
     }
+
 
 }
--- src/main/java/com/lucieyarish/usermanagementtool/models/User.java
+++ src/main/java/com/lucieyarish/usermanagementtool/models/User.java
@@ -1,4 +1,45 @@
 package com.lucieyarish.usermanagementtool.models;
 
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String surname;
+
+    @NotNull
+    private boolean active;
+
+    @NotNull
+    private String email;
+
+    private String phone;
+
+    @NotNull
+    private LocalDateTime createdOn;
+
+    public User(String name, String surname, boolean active, String email, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.active = active;
+        this.email = email;
+        this.phone = phone;
+        this.createdOn = LocalDateTime.now();
+    }
+
 }
