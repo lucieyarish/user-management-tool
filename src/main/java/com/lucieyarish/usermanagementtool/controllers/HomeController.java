10e4f6d9684a4d46d499ac80cd3daf490c636464
Lucie Yarish <lucieyarish@hotmail.com>
2022-02-23 13:55:05 +0100

    link semantic ui and create first templates

--- src/main/java/com/lucieyarish/usermanagementtool/controllers/HomeController.java
+++ src/main/java/com/lucieyarish/usermanagementtool/controllers/HomeController.java
@@ -1,32 +1,15 @@
 package com.lucieyarish.usermanagementtool.controllers;
 
-import com.lucieyarish.usermanagementtool.UserManagementToolApplication;
-import com.lucieyarish.usermanagementtool.models.User;
-import com.lucieyarish.usermanagementtool.repositories.UserRepository;
-import com.lucieyarish.usermanagementtool.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
+@Controller
 @AllArgsConstructor
 public class HomeController {
 
-    private final UserService userService;
-
-//    @GetMapping(path = "/")
-//    @ResponseBody
-//    public String index(){
-//        return "Hello World!";
-//    }
-
     @GetMapping(path = "/")
-    @ResponseBody
-    public List<User> index(){
-        List<User> users = userService.getAllUser();
-        return users;
+    public String index(){
+        return "index";
     }
 }
---
+++ src/main/java/com/lucieyarish/usermanagementtool/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.lucieyarish.usermanagementtool.controllers;
+
+import com.lucieyarish.usermanagementtool.models.User;
+import com.lucieyarish.usermanagementtool.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping(path = "/users")
+    public String users(Model model){
+        List<User> users = userService.getAllUser();
+        model.addAttribute("users", users);
+        return "users";
+    }
+}
